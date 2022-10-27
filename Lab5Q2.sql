58d3b5e4db909825be78aacf2e88ecc3aba74791
Patch <patchfact@gmail.com>
2022-10-27 14:00:40 -0700

    final

--- Lab5Q2.sql
+++ Lab5Q2.sql
@@ -1,23 +1,23 @@
 CREATE TABLE Customer (
     customerId INTEGER,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    email VARCHAR(255),
-    phoneNum VARCHAR(255),
-    address VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255),
-    postalCode VARCHAR(5),
-    country VARCHAR(255),
-    userId CHAR(8),
-    password VARCHAR(255),
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    email VARCHAR(MAX),
+    phoneNum INTEGER,
+    address VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50),
+    postalCode VARCHAR(10),
+    country VARCHAR(20),
+    userId int,
+    password VARCHAR(20),
 
     PRIMARY KEY (customerId)
 );
 
 CREATE TABLE PaymentMethod (
-    paymentMethodId CHAR(8),
-    paymentType VARCHAR(25),
+    paymentMethodId INTEGER,
+    paymentType VARCHAR(50),
     paymentNumber INTEGER,
     paymentExpiryDate DATE,
     customerId INTEGER,
@@ -30,33 +30,99 @@ CREATE TABLE OrderSummary (
     orderId INTEGER,
     orderDate DATE,
     totalAmount INTEGER,
-    shipToAddress VARCHAR(255),
-    shipToCity VARCHAR(255),
-    shipToCountry VARCHAR(255),
-    shipToPostalCode VARCHAR(5),
+    shipToAddress VARCHAR(50),
+    shipToCity VARCHAR(50),
+    shipToCountry VARCHAR(50),
+    shipToPostalCode VARCHAR(10),
     customerId INTEGER,
 
     PRIMARY KEY (orderId),
     FOREIGN KEY (customerId) REFERENCES Customer (customerId)
 );
 
-CREATE TABLE IsCart (
+CREATE TABLE InCart (
     inCartQuantity INTEGER,
     inCartPrice DECIMAL(9, 2),
     customerId INTEGER,
+    productId INTEGER,
 
-    FOREIGN KEY (customerId) REFERENCES Customer (customerId)
+    FOREIGN KEY (customerId) REFERENCES Customer (customerId),
+    FOREIGN KEY (productId) REFERENCES Product (productId)
 );
 
 CREATE TABLE Review (
     reviewId INTEGER,
     reviewRating INTEGER,
-    reviewComment VARCHAR(255),
+    reviewComment VARCHAR(1000),
     reviewDate DATE,
     customerId INTEGER,
+    productId INTEGER
 
     PRIMARY KEY (reviewId),
-    FOREIGN KEY (customerId) REFERENCES Customer (customerId)
+    FOREIGN KEY (customerId) REFERENCES Customer (customerId),
+    FOREIGN KEY (productId) REFERENCES Product (productId)
 );
- 
- somthing
+
+CREATE TABLE InShipment (
+    orderId INTEGER,
+    shipmentId INTEGER,
+
+    FOREIGN KEY (orderId) REFERENCES OrderSummary (orderId),
+    FOREIGN KEY (shipmentId) REFERENCES Shipment (shipmentId)
+);
+
+CREATE TABLE Shipment (
+    shipmentId INTEGER,
+    shipmentDate DATE,
+    shipmentDesc VARCHAR(50),
+    warehouseId INTEGER,
+
+    PRIMARY KEY (shipmentId),
+    FOREIGN KEY (warehouseId) REFERENCES Warehouse (warehouseId)
+);
+
+CREATE TABLE OrderProduct (
+    orderProductQuantity INTEGER,
+    orderProductPrice DECIMAL(9,2),
+    orderId INTEGER,
+    productId INTEGER,
+
+    FOREIGN KEY (orderId) REFERENCES OrderSummary (orderId),
+    FOREIGN KEY (productId) REFERENCES Product (productId)
+);
+
+CREATE TABLE Warehouse (
+    warehouseId INTEGER,
+    warehouseName VARCHAR(30),
+
+    PRIMARY KEY (warehouseId)
+);
+
+CREATE TABLE ProductInventory (
+    inventoryQuantity INTEGER,
+    warehouseId INTEGER,
+    productId INTEGER,
+
+    FOREIGN KEY (warehouseId) REFERENCES Warehouse (warehouseId),
+    FOREIGN KEY (productId) REFERENCES Product (productId)
+);
+
+CREATE TABLE Product (
+    productId INTEGER,
+    productName VARCHAR(50),
+    productPrice DECIMAL(9,2),
+    productImageURL VARCHAR(1000),
+    productImage VARCHAR(1000),
+    productDesc VARCHAR(1000),
+    categoryId INTEGER,
+
+    PRIMARY KEY (productId),
+    FOREIGN KEY (categoryId) REFERENCES Category (categoryId)
+);
+
+CREATE TABLE Category (
+    categoryId INTEGER,
+    categoryName VARCHAR(40),
+
+    PRIMARY KEY (categoryId)
+)
