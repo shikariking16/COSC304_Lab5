b120eea3821ada4f478b34bf13ebfcb7cf3a8870
Shikari King <108302860+shikariking16@users.noreply.github.com>
2022-10-27 13:22:21 -0700

    Add files via upload

---
+++ Lab5Q2.sql
@@ -0,0 +1,61 @@
+CREATE TABLE Customer (
+    customerId INTEGER,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(255),
+    phoneNum VARCHAR(255),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(255),
+    postalCode VARCHAR(5),
+    country VARCHAR(255),
+    userId CHAR(8),
+    password VARCHAR(255),
+
+    PRIMARY KEY (customerId)
+);
+
+CREATE TABLE PaymentMethod (
+    paymentMethodId CHAR(8),
+    paymentType VARCHAR(25),
+    paymentNumber INTEGER,
+    paymentExpiryDate DATE,
+    customerId INTEGER,
+
+    PRIMARY KEY (paymentMethodId),
+    FOREIGN KEY (customerId) REFERENCES Customer (customerId)
+);
+
+CREATE TABLE OrderSummary (
+    orderId INTEGER,
+    orderDate DATE,
+    totalAmount INTEGER,
+    shipToAddress VARCHAR(255),
+    shipToCity VARCHAR(255),
+    shipToCountry VARCHAR(255),
+    shipToPostalCode VARCHAR(5),
+    customerId INTEGER,
+
+    PRIMARY KEY (orderId),
+    FOREIGN KEY (customerId) REFERENCES Customer (customerId)
+);
+
+CREATE TABLE IsCart (
+    inCartQuantity INTEGER,
+    inCartPrice DECIMAL(9, 2),
+    customerId INTEGER,
+
+    FOREIGN KEY (customerId) REFERENCES Customer (customerId)
+);
+
+CREATE TABLE Review (
+    reviewId INTEGER,
+    reviewRating INTEGER,
+    reviewComment VARCHAR(255),
+    reviewDate DATE,
+    customerId INTEGER,
+
+    PRIMARY KEY (reviewId),
+    FOREIGN KEY (customerId) REFERENCES Customer (customerId)
+);
+
